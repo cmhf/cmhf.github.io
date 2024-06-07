41350a19863997574499afcfdd3b7b21fd5dcb98
ridndev_ <cmhfederighi@gmail.com>
2024-06-06 22:08:19 -0400

    add buttons

--- javascript/hyperlink_desmos_button.js
+++ javascript/hyperlink_desmos_button.js
@@ -1,10 +1,6 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     const desButton = document.getElementById("desmos_button");
 
-    desButton.addEventListener('click', () => {
-        
-    })
-
     desButton.classList.add("animate_flash");
     desButton.addEventListener('animationend', () => {
         desButton.classList.remove('animate_flash');
