a2eb7158c1b65ac163a7124007738993b20627b6
Colton Federighi <cmhfederighi@gmail.com>
2024-06-10 15:13:36 -0400

    Add in buttons and Desmos Subpage (Currently Broken)

--- javascript/hey.js
+++ javascript/hey.js
@@ -7,5 +7,4 @@ document.addEventListener("DOMContentLoaded", (event) => {
         Colton Federighi. */
 
     /* Your code here. */
-    
 });
--- javascript/menu_button.js
+++ javascript/menu_button.js
@@ -1,24 +1,43 @@
+out = true;
+
+function homeMenuAnimate() {
+    const cseButton = document.getElementById("essential_icon");
+    const cspButton = document.getElementById("principal_icon");
+    const csaButton = document.getElementById("application_icon");
+    const csButton  = document.getElementById("cybersecurity_icon");
+    
+    
+    if (out == false) {
+        console.log("Start anim: homeMenuAnimate() | Condition: out = true;");
+        cseButton.style.left = "50%";
+        cspButton.style.top = "40%";
+        csaButton.style.left = "50%";
+        csButton.style.top = "40%";
+        out = true;
+    } else {
+        console.log("Start anim: homeMenuAnimate() | Condition: out = false;");
+        cseButton.style.left = "15%";
+        cspButton.style.top = "15%";
+        csaButton.style.left = "86%";
+        csButton.style.top = "67%";
+        out = false;
+    }
+}
+
 function wait(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 document.addEventListener("DOMContentLoaded", async (event) => {
-    console.log("Page ready.");
 
     const mainMenuButton = document.getElementById("home_menu");
-    const cseButton = document.getElementById("essential_icon");
-    const cspButton = document.getElementById("principal_icon");
-    const csaButton = document.getElementById("application_icon");
-    const csButton  = document.getElementById("cybersecurity_icon");
-
+    console.log("Page ready.");
+    
     mainMenuButton.style.opacity = 0;
     await wait(200);
     mainMenuButton.style.opacity = 1;
 
 
-    
-    function homeMenuAnimate() {
-        console.log("Start anim: homeMenuAnimate()");
-    }
+    mainMenuButton.addEventListener("click", homeMenuAnimate);
 
 });
